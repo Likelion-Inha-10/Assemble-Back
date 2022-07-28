378f789a8e8d58e10d49b160d2bfdb2d9a66f114
jschan0911@naver.com <jschan0911@naver.com>
2022-07-28 14:46:56 +0900

    edit: allowed hosts

--- newproject/settings.py
+++ newproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gd_*c4sm+43c+m4a#hmz1r9mk6i03_$ag2m*63j-3tp8h+!cwm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.104.68.149']
+ALLOWED_HOSTS = ['172.104.68.149', '127.0.0.1']
 
 
 # Application definition
